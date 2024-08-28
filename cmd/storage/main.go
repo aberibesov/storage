24addb42b2419af2d3bb50bdf44409ece287503f
aberibesov <aberibesov@motvet.ru>
2024-08-28 11:46:03 +0300

    init commit

---
+++ cmd/storage/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"ozon/storage/internal/storage"
+)
+
+func main() {
+	st := storage.NewStorage()
+
+	file, err := st.Upload("test.txt", []byte("hello"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	restoredFile, err := st.GetById(file.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("it is restored", restoredFile)
+}
---
+++ pkg/storage/storage.go
@@ -0,0 +1,7 @@
+package storage
+
+import "ozon/storage/internal/storage"
+
+func NewStorage() *storage.Storage {
+	return storage.NewStorage()
+}
